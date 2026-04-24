afbac0f276835ed89bc4b935413de9e180f06d1c
claudiolimaluzzi-hash <dinizluzzi01@gmail.com>
2026-04-24 08:53:50 -0300

    Aula 14/04 Sobre HTML, CSS e JS

---
+++ Aula1404/nome.js
@@ -0,0 +1,9 @@
+var nome = "Claudio"
+var nome2 = prompt("Digite o seu nome: ");
+var idade = parseInt(prompt("Digite a sua idade: "));
+
+console.log("Seu nome é: ",nome)
+console.log("Sua idade é: ",idade)
+alert("Seu nome é: "+nome)
+alert("Seu nome é:"+nome2)
+alert("Sua idade é:" +idade)
---
+++ Aula1404/soma.js
@@ -0,0 +1,4 @@
+var valor1 = parseFloat(prompt("Digite um valor"))
+var valor2 = parseFloat(prompt("Digite um valor"))
+var resultado = valor1 + valor2;
+console.log("O valor da soma: ",resultado)
